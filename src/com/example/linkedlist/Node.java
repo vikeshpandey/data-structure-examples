3e5b5af4635660dacf088735f0666a20b151052b
pandey-vikesh <pandey.vikesh@gmail.com>
2017-11-19 17:04:36 +0100

    linkedlist-remove duplicates

---
+++ src/com/example/linkedlist/Node.java
@@ -0,0 +1,16 @@
+package com.example.linkedlist;
+
+
+public class Node {
+	
+		int value;
+		Node next;
+		
+		public Node(int value){
+			this.value = value;
+		}
+		
+		public void setNext(Node next){
+			this.next = next;
+		}
+}
