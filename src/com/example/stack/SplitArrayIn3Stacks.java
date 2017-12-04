f4837b2043be91b18bf5ea941841645dd0fe8ad9
c-vikpan <vikesh.pandey@assaabloy.com>
2017-12-04 16:14:23 +0100

    split array in 3 stacksa

---
+++ src/com/example/stack/SplitArrayIn3Stacks.java
@@ -0,0 +1,34 @@
+package com.example.stack;
+
+import java.util.Stack;
+
+/**
+ * Program to demonstrate splitting of array into 3 stacks
+ */
+public class SplitArrayIn3Stacks {
+    Stack<Integer> s1 = new Stack<>();
+    Stack<Integer> s2 = new Stack<>();
+    Stack<Integer> s3 = new Stack<>();
+
+    public void splitArrayIn3Stacks(int[] arr) {
+        int stackSize = arr.length / 3;
+
+        for (int i = 0, j = stackSize, k = (stackSize+stackSize),l=0; l < stackSize; i++, j++, k++, l++) {
+            s1.push(arr[i]);
+            s2.push(arr[j]);
+            s3.push(arr[k]);
+        }
+        if(arr.length%3 !=0) {
+            s3.push(arr[arr.length - 1]);
+        }
+    }
+
+    public static void main(String arg[]){
+        int arr[] = {1,2,3,4,5,6,7,8,9, 10};
+        SplitArrayIn3Stacks sa = new SplitArrayIn3Stacks();
+        sa.splitArrayIn3Stacks(arr);
+        System.out.println(sa.s1);
+        System.out.println(sa.s2);
+        System.out.println(sa.s3);
+    }
+}
