90d0e1e696d98a286c8406c794194eddb8038be4
c-vikpan <vikesh.pandey@assaabloy.com>
2017-12-05 08:37:30 +0100

    pushpopmin in 01 for stack implemented

---
+++ src/com/example/stack/Stacknode.java
@@ -0,0 +1,13 @@
+package com.example.stack;
+
+/**
+ * This represents a node in stack
+ */
+public class Stacknode<T> {
+    public int data;
+    public Stacknode<T> next;
+
+    public Stacknode(final int data) {
+        this.data = data;
+    }
+}
